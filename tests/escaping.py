d2be9ff6ec9c7adc38a9bed2e09288828baef76f
Naitik Shah <naitik.shah@yahoo.com>
2009-02-23 20:56:57 -0800

    added more unit tests from oauth.net wiki. not passing all yet

--- tests/escaping.py
+++ tests/escaping.py
@@ -1,20 +1,51 @@
 # coding: utf-8
 
 import unittest
-import urlencoding
+from urlencoding import escape
 
 
 class TestEscape(unittest.TestCase):
     def test_no_escaping(self):
-        self.assertEqual(urlencoding.escape('abcd'), 'abcd')
+        self.assertEqual(escape('abcd'), 'abcd')
 
     def test_space(self):
-        self.assertEqual(urlencoding.escape(' '), '%20')
+        self.assertEqual(escape(' '), '%20')
 
     def test_alphanumeric(self):
         alpha_num = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-        self.assertEqual(urlencoding.escape(alpha_num), alpha_num)
+        self.assertEqual(escape(alpha_num), alpha_num)
 
     def test_urlencoding_mentioned_specials(self):
         mentioned = '-._~'
-        self.assertEqual(urlencoding.escape(mentioned), mentioned)
+        self.assertEqual(escape(mentioned), mentioned)
+
+    def test_percent(self):
+        self.assertEqual(escape('%'), '%25')
+
+    def test_plus(self):
+        self.assertEqual(escape('+'), '%2B')
+
+    def test_amp(self):
+        self.assertEqual(escape('&'), '%26')
+
+    def test_equal(self):
+        self.assertEqual(escape('='), '%3D')
+
+    def test_star(self):
+        self.assertEqual(escape('*'), '%2A')
+
+    def test_line_feed(self):
+        self.assertEqual(escape(u'\u000A'), '%0A')
+
+    def test_unicode_space(self):
+        self.assertEqual(escape(u'\u0020'), '%20')
+
+    def test_seven_f(self):
+        self.assertEqual(escape(u'\u007F'), '%7F')
+
+    #FIXME: these currently fail
+    def test_eighty(self):
+        self.assertEqual(escape(u'\u0080'), '%80')
+
+    def test_three_thousand_one(self):
+        self.assertEqual(escape(u'\u3001'), '%E3%80%81')
